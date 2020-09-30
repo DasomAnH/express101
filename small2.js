ef6e7ee14ecd64c874730a4503687df83b1539b4
DasomAnH <ektha116@gmail.com>
2020-09-30 15:47:26 -0400

    small2.js updates

--- small2.js
+++ small2.js
@@ -12,15 +12,16 @@ app.get('/', (req, res) => {
   res.send('<h1>hell world</h1>');
 });
 
-app.get('/greet/Luke', (req, res) => {
-  res.send('<h2>"Hello,Luke!"<h2>');
+app.get('/greet/:name', (req, res) => {
+  res.send(`<h2>"Hello,${req.params.name}!"<h2>`);
 });
-app.get('/greet/Ahsoka', (req, res) => {
-  res.send('<h2>"Hello,Ahsoka!"<h2>');
-});
-app.get('/greet/Han', (req, res) => {
-  res.send('<h2>"Hello,Han!"<h2>');
-});
+
+// app.get('/greet/Ahsoka', (req, res) => {
+//   res.send('<h2>"Hello,Ahsoka!"<h2>');
+// });
+// app.get('/greet/Han', (req, res) => {
+//   res.send('<h2>"Hello,Han!"<h2>');
+// });
 
 server.listen(port, hostname, () => {
   // once server is listening, log to the console to say so
